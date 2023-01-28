0dbacad8e85490d0af585210c9ece7753d3e4bb9
Moss <moss@mossx.net>
2023-01-27 21:28:46 -0800

    WM: Removed Old AwesomeWM Files

--- .config/awesome/theme/init.lua
+++
@@ -1,4 +0,0 @@
-local beautiful = require("beautiful")
-local gears = require("gears")
--- Selected theme
-beautiful.init(string.format("%s/.config/awesome/theme/gruvbox/theme.lua", os.getenv("HOME")))
